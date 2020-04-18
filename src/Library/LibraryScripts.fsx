6ed7e0c00c70896924a25ae100eec74c332c5687
Richard Jackson McCann <52606032+rjmccann101@users.noreply.github.com>
2020-04-18 16:23:16 +0100

    Started work on integer partitioning

---
+++ src/Library/LibraryScripts.fsx
@@ -0,0 +1,15 @@
+﻿// Workspace for experiments on data structures
+
+
+type PartitionItem = {intVal:int; sum:int; subTree:PartitionItem list}
+
+
+let root = {intVal=0 ; sum=0; subTree =[]}
+
+let r1 = {root with subTree = [{intVal=3;sum=3;subTree=[]}]}
+r1.subTree
+
+let t1 = r1.subTree |> List.map(fun x -> {x with subTree = {intVal=2; sum=2+x.sum;subTree=[]} ::[]})
+
+let r2= {root with subTree = ({intVal=2;sum=2;subTree=[]}::t1)}
+
